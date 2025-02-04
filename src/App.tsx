8761d0f6361eca6233af3552f853359bf55c41cb
Thyelen Engel <thyengel@gmail.com>
2025-02-03 22:14:08 -0300

    organize the cards in a state

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import Card from "./components/card/Card";
 import Empty from "./components/empty-card/EmptyCard";
 
 import Column from "./components/column/Column";
+import { useState } from "react";
 
 function App() {
+  const [cards, setCards] = useState([
+    {
+      column: "toDo",
+      title: "Hey",
+      content: "Hey you",
+      labelColors: ["blue", "red"],
+    },
+    {
+      column: "inProgress",
+      title: "Hey",
+      content: "Hey you",
+      labelColors: ["black", "orange"],
+    },
+    {
+      column: "completed",
+      title: "Hey",
+      content: "Hey you",
+      labelColors: ["green", "pink"],
+    },
+  ]);
+
+  const filterToDo = cards.filter((card) => card.column === "toDo");
+  const filterInProgress = cards.filter((card) => card.column === "inProgress");
+  const filterCompleted = cards.filter((card) => card.column === "completed");
+
   return (
     <>
       <TopBar />
@@ -15,35 +41,21 @@ function App() {
         }}
       >
         <Column title="To do">
-          <Card title="Hey" content="Hey you" labelColors={["blue", "red"]} />
-          <Card
-            title="Hey again"
-            content="Hey you again"
-            labelColors={["blue", "red", "green"]}
-          />
-          <Card
-            title="Hey again"
-            content="Hey you again again"
-            labelColors={["blue", "red", "green", "pink"]}
-          />
+          {filterToDo.map(({ content, labelColors, title }) => (
+            <Card title={title} content={content} labelColors={labelColors} />
+          ))}
           <Empty />
         </Column>
         <Column title="In progress">
-          <Card title="Hey" content="Hey you" labelColors={["blue", "red"]} />
-          <Card
-            title="Hey again"
-            content="Hey you again"
-            labelColors={["blue", "red", "green"]}
-          />
+          {filterInProgress.map(({ content, labelColors, title }) => (
+            <Card title={title} content={content} labelColors={labelColors} />
+          ))}
           <Empty />
         </Column>
         <Column title="Completed">
-          <Card title="Hey" content="Hey you" labelColors={["blue", "red"]} />
-          <Card
-            title="Hey again"
-            content="Hey you again"
-            labelColors={["blue", "red", "green"]}
-          />
+          {filterCompleted.map(({ content, labelColors, title }) => (
+            <Card title={title} content={content} labelColors={labelColors} />
+          ))}
           <Empty />
         </Column>
       </div>
