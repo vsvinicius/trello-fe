a57bf3f0addf3ca12aebf4aff54462196160a816
Thyelen Engel <thyengel@gmail.com>
2025-02-14 14:29:32 -0300

    Drag and drop feature

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,29 @@ import Empty from "./components/empty-card/EmptyCard";
 
 import Column from "./components/column/Column";
 import { useState } from "react";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
 
 function App() {
   const [cards, setCards] = useState([
     {
+      id: Math.random().toString(),
       column: "toDo",
-      title: "Hey",
-      content: "Hey you",
+      title: "Task 01",
+      content: "Taks about to do something",
       labelColors: ["blue", "red"],
     },
     {
+      id: Math.random().toString(),
       column: "inProgress",
-      title: "Hey",
-      content: "Hey you",
+      title: "Task 02",
+      content: "Taks about to do something more",
       labelColors: ["black", "orange"],
     },
     {
+      id: Math.random().toString(),
       column: "completed",
-      title: "Hey",
-      content: "Hey you",
+      title: "Task 03",
+      content: "Taks about to do something a little more",
       labelColors: ["green", "pink"],
     },
   ]);
@@ -31,6 +35,7 @@ function App() {
     setCards([
       ...cards,
       {
+        id: Math.random().toString(),
         column: column,
         title: title,
         content: description,
@@ -39,17 +44,17 @@ function App() {
     ]);
   }
 
-  // const addNewCard = (column: string) => {
-  //   setCards([
-  //     ...cards,
-  //     {
-  //       column: column,
-  //       title: "Hey",
-  //       content: "Hey you",
-  //       labelColors: ["blue", "red"],
-  //     },
-  //   ]);
-  // };
+  function handleDragEnd(event: DragEndEvent) {
+    const { over } = event;
+    if (over === null) return;
+    setCards((cards) =>
+      cards.map((item) =>
+        event.active.id === item.id
+          ? { ...item, column: over.id as string }
+          : item,
+      ),
+    );
+  }
 
   const filterToDo = cards.filter((card) => card.column === "toDo");
   const filterInProgress = cards.filter((card) => card.column === "inProgress");
@@ -64,36 +69,53 @@ function App() {
           justifyContent: "center",
         }}
       >
-        <Column title="To do">
-          {filterToDo.map(({ content, labelColors, title }) => (
-            <Card title={title} content={content} labelColors={labelColors} />
-          ))}
-          <Empty
-            onAddNewCard={(title, description) =>
-              addNewCard("toDo", title, description)
-            }
-          />
-        </Column>
-        <Column title="In progress">
-          {filterInProgress.map(({ content, labelColors, title }) => (
-            <Card title={title} content={content} labelColors={labelColors} />
-          ))}
-          <Empty
-            onAddNewCard={(title, description) =>
-              addNewCard("inProgress", title, description)
-            }
-          />
-        </Column>
-        <Column title="Completed">
-          {filterCompleted.map(({ content, labelColors, title }) => (
-            <Card title={title} content={content} labelColors={labelColors} />
-          ))}
-          <Empty
-            onAddNewCard={(title, description) =>
-              addNewCard("completed", title, description)
-            }
-          />
-        </Column>
+        <DndContext onDragEnd={handleDragEnd}>
+          <Column title="To do" id="toDo">
+            {filterToDo.map(({ content, labelColors, title, id }) => (
+              <Card
+                title={title}
+                content={content}
+                labelColors={labelColors}
+                id={id}
+              />
+            ))}
+            <Empty
+              onAddNewCard={(title, description) =>
+                addNewCard("toDo", title, description)
+              }
+            />
+          </Column>
+          <Column title="In progress" id="inProgress">
+            {filterInProgress.map(({ content, labelColors, title, id }) => (
+              <Card
+                title={title}
+                content={content}
+                labelColors={labelColors}
+                id={id}
+              />
+            ))}
+            <Empty
+              onAddNewCard={(title, description) =>
+                addNewCard("inProgress", title, description)
+              }
+            />
+          </Column>
+          <Column title="Completed" id="completed">
+            {filterCompleted.map(({ content, labelColors, title, id }) => (
+              <Card
+                title={title}
+                content={content}
+                labelColors={labelColors}
+                id={id}
+              />
+            ))}
+            <Empty
+              onAddNewCard={(title, description) =>
+                addNewCard("completed", title, description)
+              }
+            />
+          </Column>
+        </DndContext>
       </div>
     </>
   );
--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,13 +1,19 @@
 import "./Column.css";
+import { useDroppable } from "@dnd-kit/core";
 
 interface ColumnProps {
   children?: React.ReactNode;
   title?: string;
+  id: string;
 }
 
-function Column({ children, title }: ColumnProps) {
+function Column({ children, title, id }: ColumnProps) {
+  const { setNodeRef } = useDroppable({
+    id: id,
+  });
+
   return (
-    <div className="column">
+    <div className="column" ref={setNodeRef}>
       <div className="cardTitle">
         <p className="titleCard">{title}</p>
       </div>
