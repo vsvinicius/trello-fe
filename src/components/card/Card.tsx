d8a23edadedb0262668256d0f94d633cacd58be9
Thyelen Engel <130775295+thyengel@users.noreply.github.com>
2025-02-18 11:22:24 -0300

    Drag and drop feature (#14)
    * Drag and drop feature
    
    * Delet the install package
    
    * Delet the install package

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,19 +3,37 @@ import message from "../../images/message.svg";
 import heart from "../../images/heart.svg";
 import clip from "../../images/clip.svg";
 import Label from "../label/Label";
+import { useDraggable } from "@dnd-kit/core";
 
 interface CardProps {
   title?: string;
   content?: string;
   labelColors?: string[];
+  id: string;
 }
 
-function Card({ content, title, labelColors = [] }: CardProps) {
+function Card({ id, content, title, labelColors = [] }: CardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: id,
+  });
+
+  const style = transform
+    ? {
+        transform: `translate(${transform.x}px, ${transform.y}px)`,
+      }
+    : undefined;
+
   const labels = labelColors.map((labelColor) => (
     <Label color={labelColor} key={labelColor} />
   ));
   return (
-    <div className="rectangle">
+    <div
+      className="rectangle"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
       <div style={{ display: "flex" }}>{labels}</div>
       <div style={{ marginTop: "20px" }}>
         <div className="title">
